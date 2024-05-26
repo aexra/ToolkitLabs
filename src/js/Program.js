98fc57bba8f5ddaefc8e0146b80a00776d2e70f4
Никита Фомин <defdXs@yandex.ru>
2024-05-26 20:13:00 +0300

    Some really weird things

---
+++ src/js/Program.js
@@ -0,0 +1,26 @@
+import { BrowserWindow } from 'electron';
+
+export class Program {
+    constructor(t, w, h, mv, mhp, ip) {
+        this.title = t;
+        this.width = w;
+        this.height = h;
+        this.menuVisible = mv;
+        this.mainHtmlPath = mhp;
+        this.iconPath = ip;
+        this.window = null;
+    }
+    run() {
+        this.window = new BrowserWindow({
+            width: this.width,
+            height: this.height,
+            icon: this.iconPath,
+            title: this.title
+        })
+        this.window.setMenuBarVisibility(this.menuVisible);
+        this.window.loadFile(this.mainHtmlPath);
+    }
+    quit() {
+
+    }
+}
---
+++ src/js/start.js
@@ -0,0 +1,21 @@
+import { Program } from "./Program.js";
+import { app } from 'electron';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+var program = new Program(
+    "Toolkit Labs",
+    300, 500,
+    false,
+    'src/html/index.html',
+    path.join(__dirname, '../assets/logo.png')
+);
+
+app.whenReady().then(() => program.run());
+app.on('window-all-closed', () => {
+    program.quit();
+    app.quit();
+});
